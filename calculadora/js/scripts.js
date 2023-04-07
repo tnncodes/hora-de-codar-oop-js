a1ee9eaddaaaee6706c09ebba6c32ebad5b194c6
tnncodes <tnncodes@gmail.com>
2023-04-07 18:30:55 -0300

    calculadora: criando a lógica dos botões

--- calculadora/js/scripts.js
+++ calculadora/js/scripts.js
@@ -6,8 +6,35 @@ class Calculator {
     this.reset = 0;
   }
 
+  checkLastDigit(input, upperValue, reg) {
+    if((
+      !reg.test(input) &&
+      !reg.test(upperValue.substr(upperValue.length - 1))
+    )) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   btnPress() {
-    console.log('clicou');
+    let input = this.textContent;
+    let upperValue = calc.upperValue.textContent;
+    
+    // verifica se tem somente números
+    const reg = new RegExp('^\\d+$');
+
+    // verifica se recisa adicionar ou não
+    if(calc.checkLastDigit(input, upperValue, reg)) {
+      return false;
+    }
+
+
+    if(upperValue === '0') {
+      calc.upperValue.textContent = input;
+    } else {
+      calc.upperValue.textContent += input;
+    }
   }
 }
 
