59ba711bb933cabfe0e0aea63960bda1fa5f5f86
tnncodes <tnncodes@gmail.com>
2023-04-07 18:10:22 -0300

    calculadora: criando a Class da calculadora

--- calculadora/js/scripts.js
+++ calculadora/js/scripts.js
@@ -1,1 +1,23 @@
-console.log('certo');
+class Calculator {
+  // O método constructor é executado quando o objeto é instanciado 
+  constructor() {
+    this.upperValue = document.querySelector('#upper-number');
+    this.resultValue = document.querySelector('#result-number');
+    this.reset = 0;
+  }
+
+  btnPress() {
+    console.log('clicou');
+  }
+}
+
+// start obj
+let calc = new Calculator;
+
+// start btns
+let buttons = document.querySelectorAll('.btn');
+
+// map all buttons
+for(let i = 0; buttons.length > i; i++) {
+  buttons[i].addEventListener('click', calc.btnPress);
+}
